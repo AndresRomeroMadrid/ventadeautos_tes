d96f58232cc4349df0a310eecb616a21f3352b55
AndresRomeroMadrid <aromero.madrid@gmail.com>
2025-04-30 17:29:26 -0400

    Update AutoController.java

--- AutoController.java
+++ AutoController.java
@@ -1,5 +1,5 @@
 package com.Automotora.ventadeautos.Auto;
-
+//se prueba modificar
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
